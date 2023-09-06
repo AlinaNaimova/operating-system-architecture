6557c4f888c2a37415ab25e82c1338307acfbb29
Alina N <my_work_space@bk.ru>
2023-09-06 16:28:57 +0300

    deleted unnecessary comments

--- seminar4shapes/src/shapes/Quadrilateral.java
+++ seminar4shapes/src/shapes/Quadrilateral.java
@@ -2,16 +2,9 @@ package shapes;
 
 public class Quadrilateral implements IShape {
     private double side;
-//    private double side2;
-//    private double side3;
-//    private double side4;
-
 
     public Quadrilateral(double side) {
         this.side = side;
-//        this.side2 = side2;
-//        this.side3 = side3;
-//        this.side4 = side4;
     }
 
     @Override
